53eb85621b51a925cb236217bed3c62c50305207
maciunkax3 <maciekplewka@wp.pl>
2019-02-12 15:57:30 +0100

    szablon + piosenki

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using projektGrupowy.Models;
+
+namespace projektGrupowy.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Piosenki Zespołu Michał Kołpuć";
+            var model = new List<projektGrupowy.Models.MusicFile>();
+            List<string> files = new List<string>();
+            files.Add("MchalKolpucPiosenkaOMilosciNieDoKoncaHeteroSeksualnej.mp3");
+            files.Add("MichalKolpucTygrysToJa.mp3");
+            files.Add("MichalKolpucJebacPb.mp3");
+            files.Add("BohdanSmoleńUBodziaDisco.mp3");
+            foreach (var file in files)
+            {
+                model.Add(new projektGrupowy.Models.MusicFile() { path = "aaa", fileName = file });
+            }
+            return View(model);
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/MusicFile.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace projektGrupowy.Models
+{
+    public class MusicFile
+    {
+        public string path { get; set; }
+        public string fileName { get; set; }
+
+    }
+}
